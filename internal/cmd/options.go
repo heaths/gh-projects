8e25bb5b79db7c5943bc5feadce5775eca042322
Heath Stewart <heaths@outlook.com>
2022-05-02 23:09:49 -0700

    Support reading body from stdin via -
    Fixes #15

--- internal/cmd/options.go
+++ internal/cmd/options.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"strconv"
 	"strings"
 
 	"github.com/cli/go-gh/pkg/repository"
 	"github.com/heaths/go-console"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 type GlobalOptions struct {
@@ -15,6 +17,48 @@ type GlobalOptions struct {
 
 	Repo    repository.Repository
 	Verbose bool
+}
+
+func StdinStringVarP(flags *pflag.FlagSet, stdin io.Reader, p *string, name, shorthand, value, usage string) {
+	flags.VarP(newStdinValue(stdin, p, value), name, shorthand, usage)
+}
+
+type stdinValue struct {
+	stdin io.Reader
+	value *string
+}
+
+func newStdinValue(stdin io.Reader, p *string, value string) *stdinValue {
+	val := stdinValue{
+		stdin: stdin,
+		value: p,
+	}
+
+	*val.value = value
+	return &val
+}
+
+func (v *stdinValue) String() string {
+	return string(*v.value)
+}
+
+func (v *stdinValue) Set(s string) error {
+	if s == "-" {
+		stdin, err := io.ReadAll(v.stdin)
+		if err != nil {
+			return fmt.Errorf("failed to read from STDIN: %w", err)
+		}
+
+		*v.value = string(stdin)
+	} else {
+		*v.value = s
+	}
+
+	return nil
+}
+
+func (v *stdinValue) Type() string {
+	return "string"
 }
 
 func projectNumber(number *uint32) cobra.PositionalArgs {
